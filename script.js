b70c6ac3af7b03cd2fdcce09c4a25b1d35194a55
Amaobi Amaobi Obikobe <obikobea@accessbankplc.com>
2020-07-15 16:51:38 +0100

    init project

---
+++ script.js
@@ -0,0 +1,45 @@
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+dotenv.config();
+const filename = "web.config";
+
+const copyFilesToWebconfig = () => {
+  try {
+    const data = fs.readFileSync(`${process.env.FILE_PATH}.env`, "utf-8");
+    const lines = data.split("\n");
+    let output = "";
+    for (line of lines) {
+      if (line.charAt(0) === "#") continue;
+
+      if (!line.includes("=")) continue;
+
+      const entry = line.split("=");
+      const key = entry[0];
+      const value = (entry[1]).replace(/(?:\r\n|\r|\n)/g, "").trim();
+      output += `<add key="${key.trim()}" value="${value}"/> \n`;
+    }
+    writeToFile(output);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+};
+
+const writeToFile = (word) => {
+  let buffer = new Buffer.from(word);
+
+  fs.open(filename, "w", function (err, fd) {
+    if (err) {
+      throw "could not open file: " + err.message;
+    }
+
+    // write the contents of the buffer, from position 0 to the end, to the file descriptor returned in opening our file
+    fs.write(fd, buffer, 0, buffer.length, null, function (err) {
+      if (err) throw "error writing file: " + err;
+      fs.close(fd, function () {
+        console.log("Done. Browse "+filename);
+      });
+    });
+  });
+};
+copyFilesToWebconfig();
